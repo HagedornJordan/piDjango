fd7bb4f001fa0c0d7445d0c943baaa221ee0f21e
Jordan Hagedorn <hagedornjordan@gmail.com>
2021-09-27 22:44:17 -0400

    update blink

--- piDjango/quickstart/blink.py
+++ piDjango/quickstart/blink.py
@@ -1,11 +1,15 @@
 # Test program to ensure your GPIO is correct. 
 import RPi.GPIO as GPIO
 import time
-
+sleepTime = .25
+blinks = 10
 def blinkTest():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(23, GPIO.OUT)
-    GPIO.output(23, GPIO.HIGH)
-    time.sleep(20)
-    GPIO.output(23, GPIO.LOW)
+    for i in range(blinks):
+        GPIO.output(23, GPIO.HIGH)
+        time.sleep(sleepTime)
+        GPIO.output(23, GPIO.LOW)
+        time.sleep(sleepTime)
+        
     GPIO.cleanup()
